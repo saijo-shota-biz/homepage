31f8f66d28284e21bb564c592c6ef826206b2afe
saijo.shota.biz <saijo.shota.biz@gmail.com>
2022-12-27 02:19:10 +0900

    fix: home-og delte twemoji

--- src/pages/api/home-og.tsx
+++ src/pages/api/home-og.tsx
@@ -44,7 +44,6 @@ export default async function handler(req: NextRequest) {
     {
       width: 1200,
       height: 630,
-      emoji: 'twemoji',
       fonts: [
         {
           name: 'MPLUSRounded1c',
