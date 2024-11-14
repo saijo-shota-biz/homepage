3d499bedc0a8b9d27e38974664f2ae071c0d583f
saijo.shota <saijo.shota.biz@gmail.com>
2024-11-15 00:14:48 +0900

    ✨ feat(routes): add loader for fetching markdown content from GitHub using Octokit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,35 @@
+import { Link, useLoaderData } from '@remix-run/react';
 import type { MetaFunction } from '@vercel/remix';
+import { Octokit } from 'octokit';
 
 export const meta: MetaFunction = () => {
   return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
 };
 
+export const loader = async () => {
+  const octokit = new Octokit();
+  const response = await octokit.request("GET /repos/{owner}/{repo}/git/trees/{tree_sha}", {
+    owner: "saijo-shota-biz",
+    repo: "saijo-shota-biz",
+    tree_sha: "main",
+  });
+
+  return { list: response.data.tree };
+};
+
 export default function Index() {
-  return <div>test</div>;
+  const { list } = useLoaderData<typeof loader>();
+  return (
+    <ul>
+      {list
+        .filter((item) => item.path !== "README.md")
+        .map((item) => {
+          return (
+            <li key={item.sha}>
+              <Link to={`/${item.sha}`}>{item.path?.split(".")[1]}</Link>
+            </li>
+          );
+        })}
+    </ul>
+  );
 }
