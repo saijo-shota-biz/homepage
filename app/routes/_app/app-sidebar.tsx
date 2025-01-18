8047542fa6ad21772ab5ebb2591f5fd61fc9d3ce
saijo.shota <saijo.shota.biz@gmail.com>
2025-01-18 19:00:06 +0900

    - noteへのリンクを追加

--- app/routes/_app/app-sidebar.tsx
+++ app/routes/_app/app-sidebar.tsx
@@ -1,6 +1,5 @@
 import { Link, useLocation } from "@remix-run/react";
-import type { LucideProps } from "lucide-react";
-import { Code, Home, Layers3, Lightbulb, NotebookPen, User } from "lucide-react";
+import { Code, Home, Layers3, Lightbulb, type LucideProps, NotebookPen, User } from "lucide-react";
 import { type ForwardRefExoticComponent, type RefAttributes, useEffect } from "react";
 import {
   Sidebar,
@@ -26,6 +25,16 @@ const Twitter = () => (
   <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <title>X</title>
     <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+  </svg>
+);
+
+const Note = () => (
+  <svg role="img" viewBox="0 0 493 493" xmlns="http://www.w3.org/2000/svg">
+    <title>Note</title>
+    <path
+      style={{ fill: "#040000" }}
+      d="m139.57,142.06c41.19,0,97.6-2.09,138.1-1.04,54.34,1.39,74.76,25.06,75.45,83.53.69,33.06,0,127.73,0,127.73h-58.79c0-82.83.35-96.5,0-122.6-.69-22.97-7.25-33.92-24.9-36.01-18.69-2.09-71.07-.35-71.07-.35v158.96h-58.79v-210.22Z"
+    />
   </svg>
 );
 
@@ -101,6 +110,12 @@ const itemGroups = [
         title: "X(Twitter)",
         url: "https://x.com/saijo_shota",
         icon: Twitter,
+      },
+      {
+        key: "note",
+        title: "note",
+        url: "https://note.com/saijo_shota",
+        icon: Note,
       },
     ],
   },
